48508b2ef73cdb5e9e3c3f601bb90c6e2c31ac96
zeris <brandon-gg1@outlook.com>
2018-10-21 21:10:04 -0500

    Added main menu UI icons
    Agrefado todos los iconos del menu principal.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 
+	[Header ("Setting's buttons")]
+	public Sprite soundON;
+	public Sprite soundOFF;
+	public Button soundBtn;
+	public Text soundBtnText;
+
+	private static bool sound = true;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,5 +25,20 @@ public class MainMenuManager : MonoBehaviour {
 	public void OpenLinks(string link)
 	{
 		Application.OpenURL (link);
+	}
+
+	public void SoundBtnOnClick()
+	{
+		sound = (!sound);
+		if(sound == true)
+		{
+			soundBtn.GetComponent<Image>().sprite = soundON;
+			soundBtnText.color = Color.black;
+		}
+		else
+		{
+			soundBtn.GetComponent<Image>().sprite = soundOFF;
+			soundBtnText.color = Color.white;
+		}
 	}
 }
